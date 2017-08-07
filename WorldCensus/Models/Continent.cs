462f9484609b72d4d5ce83cce30d2468f7a0dc5d
Tim Wyrick <twyrick94@gmail.com>
2017-08-07 12:37:43 -0500

    Initial Commit

---
+++ WorldCensus/Models/Continent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorldCensus.Models
+{
+    public class Continent
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Country> Countries { get; set; }
+    }
+}
---
+++ WorldCensus/Models/WorldCensusContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+
+namespace WorldCensus.Models
+{
+    public class WorldCensusContext : DbContext
+    {
+        public DbSet<Continent> Continents { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
